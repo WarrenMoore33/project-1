505760c70309e5e69081d20aba939953dc692508
WarrenMoore33 <WarrenMoore33@users.noreply.github.com>
2017-12-26 14:20:00 -0500

    adding the tags
    adding the tags property. also added a few quotes

--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 //Creating the QUOTES variable as an array of objects
 //Each object holds a quote, the source, an optional citation, and an optional year
 var quotes = [
-    {quote: "HANDICAP to mr me, never told a PARALYZE still here and the WHEEL CHAIR for DISABILITY", source: "Handlez Mcgee", citation:"The Arena Mixtape", year: 2013},
-    {quote: "Safe sex is great sex, you better wear a latex, 'cause you don't want that late text that...I think I'm late text...so wrap it up", source: "Lil Wayne", citation:"Lollipop Remix", year: 2008},
-    {quote: "Feel it way down in my CELL all I need is one MICRO-PHONE", source: "Handlez Mcgee", citation:"aMC Mixtape", year: 2014},
+    {quote: "HANDICAP to mr me, never told a PARALYZE still here and the WHEEL CHAIR for DISABILITY", source: "Handlez Mcgee", citation:"The Arena Mixtape", year: 2013, tags:"Music"},
+    {quote: "Safe sex is great sex, you better wear a latex, 'cause you don't want that late text that...I think I'm late text...so wrap it up", source: "Lil Wayne", citation:"Lollipop Remix", year: 2008, tags:"Music"},
+    {quote: "Feel it way down in my CELL all I need is one MICRO-PHONE", source: "Handlez Mcgee", citation:"aMC Mixtape", year: 2014, tags:"Music"},
     {quote: "Revenge is a dish best served cold...", source: "Unknown"},
-    {quote: "The elephant in the room is never mentioned...because that room is too full to fit anyone else.", source: "Me", citation:"This Page", year: 2017}
+    {quote: "The elephant in the room is never mentioned...because that room is too full to fit anyone else.", source: "Me", citation:"This Page", year: 2017, tags:"Comedy"},
+    {quote: "You cannot shake hands with a clenched fist.", source: "Indira Gandhi", tags: "Inspiration"},
+    {quote: "There is nothing permanent except change.", source: "Heraclitus"},
+    {quote: "All that we see or seem is but a dream within a dream.", source: "Edgar Allan Poe", year: 1849}
+    
 ];
 
 //Function to pull a quote form the array QUOTES randomly
@@ -48,6 +52,10 @@ function printQuote() {
         message += '<span class="year">' + newQuote.year + '</span>';
     }
     
+    message += '</p>';
+    if (newQuote.tags) {
+        message += '<p class="tags">' + newQuote.tags + '</p>';
+    }
     
     document.getElementById('quote-box').innerHTML = message;
 
@@ -55,9 +63,10 @@ function printQuote() {
     randomColor();
 }
 
+//Function to automatically generate quotes and backgrounds if the user does not hit the button
 var myVar;
 
 function automaticRandomizer() {
-    myVar = setInterval(printQuote, 5000);
+    myVar = setInterval(printQuote, 10000);
 }
 automaticRandomizer();
